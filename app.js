b0d952741bde903f80aa58ba12d8a0e9a9395482
Shivani Agarwal <shiviagarwal6161@gmail.com>
2024-10-02 19:22:06 +0530

    Fix: Conflict

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:3000`);
+    console.log(`Server running on http://localhost:${port}`);
 });
